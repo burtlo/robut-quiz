a51abc9de543d3ae041d0f9daee6d8f2f50dbee1
Franklin Webber <franklin.webber@gmail.com>
2011-10-14 13:24:06 -0700

    Question will find the parameters successfully

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -4,19 +4,23 @@ require 'robut'
 class Robut::Plugin::Quiz
   include Robut::Plugin
   
-  # @see http://rubular.com?regex=ask%20%3F(choice%7Cpolar%7Cscale)%3F%20(%3F%3Aquestion%20)%3F'(%5B%5E'%5D%2B)'%5B%5Cs%2C%5D*(%3F%3A'(%5B%5E'%5D%2B)'%5B%5Cs%2C%5D*)*(%3F%3A(%3F%3Afor%20)%3F(%5Cd%2B)%20minutes%3F)%3F
+  # @see ask%20%3F(choice%7Cpolar%7Cscale)%3F%20(%3F%3Aquestion%20)%3F'(%5B%5E'%5D%2B)'%5B%5Cs%2C%5D*((%3F%3A'%5B%5E'%5D%2B'%5B%5Cs%2C%5D*)*)%2B(%3F%3A(%3F%3Afor%20)%3F(%5Cd%2B)%20minutes%3F)%3F
   # 
   # 1st: question type - choice, polar, scale
   # 2nd: question
   # 3rd: choices 
   # 
-  QUESTION_REGEX = /^ask ?(choice|polar|scale)? (?:question )?'([^']+)'[\s,]*(?:'([^']+)'[\s,]*)*(?:(?:for )?(\d+) minutes?)?$/
+  QUESTION_REGEX = /^ask ?(choice|polar|scale)? (?:question )?'([^']+)'[\s,]*((?:'[^']+'[\s,]*)*)*(?:(?:for )?(\d+) minutes?)?$/
   
+  # This regex will find all the questions and parameters specified with the question 
+  GROUP_REGEX = /'([^']+)'/ 
+  
+
   def usage
     [ 
-      "#{at_nick} ask choice (question) 'What do you want for lunch?', 'pizza', 'sandwich', 'salad' for 1 minute",
-      "#{at_nick} ask polar (question) 'Should I continue the presentation?' for 3 minutes",
-      "#{at_nick} ask scale (question) 'how much did you like the presentation?', '1..5'",
+      "#{at_nick} ask choice question 'What do you want for lunch?', 'pizza', 'sandwich', 'salad' for 1 minute",
+      "#{at_nick} ask polar 'Should I continue the presentation?' for 3 minutes",
+      "#{at_nick} ask scale 'how much did you like the presentation?', '1..5'",
       "#{at_nick} ask 'Should I continue the presentation?', 'y|yes', 'n|no' for 1 minutes"
     ]
   end
@@ -27,14 +31,23 @@ class Robut::Plugin::Quiz
     request = words(message).join(" ")
     
     if sent_to_me? message and is_a_valid_question? request
+      
       request =~ QUESTION_REGEX
       
       question_type = Regexp.last_match(1) || 'choice'
       question = Regexp.last_match(2)
-      parameters = Regexp.last_match(3)
-      answer_length = Regexp.last_match(4) || '2'
+      answer_length = Regexp.last_match(4) || '2 minutes'
+
+      # After the target, question type, question, and answer length has been determined
+      # look at all the single quoted items to find the list of parameters if there are any
+      parameters = request.scan(GROUP_REGEX)[1..-1].flatten
       
-      puts %{ #{question_type} }
+      puts "Request: #{request}"
+      puts %{ 
+        type:       #{question_type}
+        questions:  #{question}
+        parameters: #{parameters} 
+        time:       #{answer_length} }
       
       case question_type
       when 'polar'
--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -13,7 +13,8 @@ describe Robut::Plugin::Quiz do
     "ask choice 'What do you want for lunch?', 'pizza', 'sandwich', 'salad' for 1 minute",
     "ask polar 'Should I continue the presentation?' for 3 minutes",
     "ask scale question 'how much did you like the presentation?', '1..5'",
-    "ask 'Should I continue the presentation?', 'y|yes', 'n|no' for 1 minutes"
+    "ask 'Should I continue the presentation?', 'y|yes', 'n|no' for 1 minutes",
+    "ask polar 'Should I continue the presentation?' for 3 minutes"
     
   ].each do |question|
   
@@ -24,14 +25,14 @@ describe Robut::Plugin::Quiz do
     end
     
   end
-  
+    
   describe "#handle" do
     
     context "when a polar question is asked" do
       
       it "should find all the components of the question" do
-
-        subject.should_receive(:handle_polar).with('Should I continue the presentation?',nil,'3')
+  
+        subject.should_receive(:handle_polar).with('Should I continue the presentation?',[],'3')
         subject.handle Time.now,'person',"@quizmaster ask polar 'Should I continue the presentation?' for 3 minutes"
         
       end
@@ -42,8 +43,8 @@ describe Robut::Plugin::Quiz do
       
       it "should find all the components of the question" do
         
-        subject.should_receive(:handle_choice).with('What should I talk about next?',"'x', 'y', 'z'",'2')
-        subject.handle Time.now,'person',"@quizmaster ask choice 'What should I talk about next?', 'x', 'y', 'z'"
+        subject.should_receive(:handle_choice).with('What should I talk about next?',['x', 'y', 'z'],'4')
+        subject.handle Time.now,'person',"@quizmaster ask choice 'What should I talk about next?', 'x', 'y', 'z' for 4 minutes"
         
       end
       
